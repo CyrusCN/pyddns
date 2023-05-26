eea7594c2c26cbd16c555c148e6880eff213ec19
Cyrus Shi <108404529+CyrusCN@users.noreply.github.com>
2023-05-26 20:06:59 +0800

    no

--- ddns.py
+++ ddns.py
@@ -16,4 +16,4 @@ value = valuev6[stloc:endloc]
 print (value)
 #curl a str value from ipaddr6
 #以下post包请根据对应dns解析商自行构建
-os.system("curl -X POST https://dnsapi.cn/Record.Ddns -d 'login_token=,,&value=%s'" % value)>
+os.system("curl -X POST https://dnsapi.cn/Record.Ddns -d 'login_token=,,&value=%s'" % value)
